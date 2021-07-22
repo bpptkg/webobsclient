fe529d3e197f0b0cd5e31ee37e0f43a3dac184c9
Indra Rudianto <indrarudianto.official@gmail.com>
2021-07-22 17:00:05 +0700

    Remove unused import

--- webobsclient/client.py
+++ webobsclient/client.py
@@ -1,6 +1,5 @@
 from webobsclient.auth import BasicAuthenticationAPI
-from webobsclient.request import (BasicRequest, MC3RequestMethod,
-                                  Sefran3RequestMethod)
+from webobsclient.request import MC3RequestMethod, Sefran3RequestMethod
 
 __all__ = [
     'MC3Client',
